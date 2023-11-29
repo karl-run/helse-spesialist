b6ca8678cedaf0b0773a905894a5e805f7c5b435
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-11-29 11:41:49 +0100

    ⚗️ Legger til gradlePluginPortal
    Kanskje det gjør at gradle-dependency-submission skjønner hvor
    graphql-kotlin-gradle-plugin fins?

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "spesialist"
+
 include("spesialist-felles", "spesialist-api", "spesialist-selve", "spesialist-opprydding-dev", "spesialist-migrering", "spesialist-modell")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
